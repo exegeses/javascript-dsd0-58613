034b719ec8c02f20d058630eb2b5d4c2dade29c6
Marcos <exegeses@gmail.com>
2022-07-23 17:26:02 -0300

    variables

---
+++ clase2/js/usuario.js
@@ -0,0 +1,9 @@
+/*
+pedimos al usuario que ingrese su nombre
+    almacenamos el datos en una variable
+    lego imprimimos dato en el documento
+*/
+
+let nombre = prompt('Ingrese su nombre');
+
+document.write( 'bienvenido '+ nombre );
---
+++ clase2/js/variables.js
@@ -0,0 +1,12 @@
+//alert('aggg');//interrupción
+let num = 147;
+const nombre = 'marcos'; // string 
+
+document.write( num );
+document.write( '<br>' );
+document.write( nombre );
+/* no puede haber sobreescritura en una contante
+nombre = 'azael';
+document.write( '<br>' );
+document.write( nombre );
+*/
