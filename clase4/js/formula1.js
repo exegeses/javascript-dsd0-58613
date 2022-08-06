76aeed3afcfdc07de6321a94ced33ec105ddd77d
Marcos <exegeses@gmail.com>
2022-08-06 16:26:02 -0300

    práctica F1 usando if/else if

--- clase4/js/formula1.js
+++ clase4/js/formula1.js
@@ -11,6 +11,28 @@ mclaren.style.display = 'none';
 
 function mostrarOcultar( team )
 {
-    ferrari.style.display = 'flex';
-    redbull.style.display = 'none';
+    if ( team == 'redbull' ){
+        redbull.style.display = 'flex';
+        ferrari.style.display = 'none';
+        mercedes.style.display = 'none';
+        mclaren.style.display = 'none';
+    }
+    else if ( team == 'ferrari' ){
+        redbull.style.display = 'none';
+        ferrari.style.display = 'flex';
+        mercedes.style.display = 'none';
+        mclaren.style.display = 'none';
+    }
+    else if ( team == 'mercedes' ){
+        redbull.style.display = 'none';
+        ferrari.style.display = 'none';
+        mercedes.style.display = 'flex';
+        mclaren.style.display = 'none';
+    }
+    else if ( team == 'mclaren' ){
+        redbull.style.display = 'none';
+        ferrari.style.display = 'none';
+        mercedes.style.display = 'none';
+        mclaren.style.display = 'flex';
+    }
 }
