7180973d569c517a447356a0acfd1193085c05ce
Marcos <exegeses@gmail.com>
2022-08-06 18:06:35 -0300

    innerHTML

---
+++ clase4/js/innerHTML.js
@@ -0,0 +1,3 @@
+let txt = document.querySelector('#txt');
+
+txt.innerHTML = '<img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2F9e%2F34%2F87%2F9e3487c125dadd28d2175f49b0332106.jpg&f=1&nofb=1">';
