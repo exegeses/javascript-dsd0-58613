df8da47306302ef2df4b7f32c7e0636d292a6b54
Marcos <exegeses@gmail.com>
2022-08-06 15:57:16 -0300

    material práctica fórmula 1

---
+++ clase4/js/formula1.js
@@ -0,0 +1,10 @@
+let redbull = document.querySelector('#redbull');
+let ferrari = document.querySelector('#ferrari');
+let mercedes = document.querySelector('#mercedes');
+let mclaren = document.querySelector('#mclaren');
+
+
+/* estado inicial */
+ferrari.style.display = 'none';
+mercedes.style.display = 'none';
+mclaren.style.display = 'none';
